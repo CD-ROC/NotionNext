47357101b8f611206306169dfdc60f698565f0ce
CD-ROC <160688438+CD-ROC@users.noreply.github.com>
2024-05-23 12:00:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,14 +19,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是Paul，来自中国四川成都。,生命这样的旅程，我渴求你的微笑来完整。欢迎来到我的网站！我将与大家分享我的科技、旅行、理财的见闻和心得，同时也会探讨成都生活吃喝玩乐相关的话题。,The new born Paul BLOG🎉',
+    'Hi，我是李，来自中国四川成都。,生命这样的旅程，我渴求你的微笑来完整。欢迎来到我的网站！我将与大家分享我的科技、旅行、理财的见闻和心得，同时也会探讨成都生活吃喝玩乐相关的话题。🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Paul', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '独李思考', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'BLOG', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.552355.zxy', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Paul, BLOG', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'LI, BLOG', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'paul@552355.xyz', // 邮箱地址 例如mail@tangly1024.com
